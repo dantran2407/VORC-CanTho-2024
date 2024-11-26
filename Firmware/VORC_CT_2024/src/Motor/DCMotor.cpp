667b8b8bf427c23beda9729d4656e8b6e2cef3ed
DanTran <danmc907@gmail.com>
2024-11-26 10:36:56 +0700

    Update Soft Starter And Soft Stopper

--- Firmware/VORC_CT_2024/src/Motor/DCMotor.cpp
+++ Firmware/VORC_CT_2024/src/Motor/DCMotor.cpp
@@ -1,5 +1,6 @@
 #include <DCMotor.h>
 #include <variable.h>
+#include <Arduino.h>
 
 using namespace DCMotor;
 
@@ -34,36 +35,87 @@ void MotorController::setPIN(int speed, bool direction)
     }
 }
 
-void MotorController::_softStarter(int speed, bool direction, void (*func)(int, bool))
-{
+// void MotorController::_softStarter(int speed, bool direction, void (*func)(int, bool))
+// {
+//     int tSpeed = 0;
+
+//     for (int dutyCycle = 0; dutyCycle <= MAXDUTYCYCLE; dutyCycle += INCREMENT)
+//     {
+//         tSpeed = map(dutyCycle, 0, MAXDUTYCYCLE, _currentSpeed, speed);
+//         func(tSpeed, direction);
+//         delay(DUTYCYCLESTEP);
+//     }
+
+//     _currentSpeed = speed;
+//     _currentDirection = direction;
+// }
+
+/* UPDATE NEW TIMER - SOFT STARTER USING MILLIS AND YIELD */
+void MotorController::_softStarter(int speed, bool direction, void (*func)(int, bool)) {
+    unsigned long previousMillis = millis();  // Store the current time
     int tSpeed = 0;
 
-    for (int dutyCycle = 0; dutyCycle <= MAXDUTYCYCLE; dutyCycle += INCREMENT)
-    {
+    for (unsigned int dutyCycle = 0; dutyCycle <= MAXDUTYCYCLE; dutyCycle += INCREMENT) {
+        while (millis() - previousMillis < DUTYCYCLESTEP) {
+            yield();  // Allow other tasks to run
+        }
+
+        previousMillis += DUTYCYCLESTEP;  // Update for the next cycle
+
+        // Calculate speed based on duty cycle
         tSpeed = map(dutyCycle, 0, MAXDUTYCYCLE, _currentSpeed, speed);
+        
+        // Call the provided function pointer to set motor speed
         func(tSpeed, direction);
-        delay(DUTYCYCLESTEP);
     }
 
+    // Update the motor's current state
     _currentSpeed = speed;
     _currentDirection = direction;
 }
 
-void MotorController::_softStopper(void (*func)(int, bool))
-{
+// void MotorController::_softStopper(void (*func)(int, bool))
+// {
+//     int tSpeed = 0;
+
+//     for (int dutyCycle = MAXDUTYCYCLE; dutyCycle >= 0; dutyCycle -= INCREMENT)
+//     {
+//         tSpeed = map(dutyCycle, 0, MAXDUTYCYCLE, 0, _currentSpeed);
+//         func(tSpeed, _currentDirection);
+//         delay(DUTYCYCLESTEP);
+//     }
+
+//     func(0, _currentDirection);
+
+//     _currentSpeed = 0;
+//     _currentDirection = true;
+// }
+
+/* UPDATE NEW TIMER - SOFT STOPPER USING MILLIS AND YIELD*/
+void MotorController::_softStopper(void (*func)(int, bool)) {
+    unsigned long previousMillis = millis();  // Store the current time
     int tSpeed = 0;
 
-    for (int dutyCycle = MAXDUTYCYCLE; dutyCycle >= 0; dutyCycle -= INCREMENT)
-    {
-        tSpeed = map(dutyCycle, 0, MAXDUTYCYCLE, 0, _currentSpeed);
-        func(tSpeed, _currentDirection);
-        delay(DUTYCYCLESTEP);
+    // Loop to gradually decrease speed from current to zero
+    for (unsigned int dutyCycle = MAXDUTYCYCLE; dutyCycle > 0; dutyCycle -= INCREMENT) {
+        if (millis() - previousMillis >= DUTYCYCLESTEP) {
+            previousMillis += DUTYCYCLESTEP;  // Move to the next interval
+
+            // Calculate speed based on duty cycle
+            tSpeed = map(dutyCycle, 0, MAXDUTYCYCLE, 0, _currentSpeed);
+            
+            // Call the provided function pointer to set motor speed
+            func(tSpeed, _currentDirection);
+        }
+        yield();  // Allow other tasks to run
     }
 
-    setPIN(0, _currentDirection);
+    // Ensure motor is completely stopped at the end
+    func(0, _currentDirection);
 
+    // Update the motor's state
     _currentSpeed = 0;
-    _currentDirection = true;
+    _currentDirection = true;  // Default direction (optional)
 }
 
 void MotorController::setSpeed(int speed, bool direction, void (*func)(int, bool))
